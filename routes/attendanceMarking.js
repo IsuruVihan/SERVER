daca10d064c7e36360782cad48d12c5acd46b9dc
Avishna Arambawela <111332549+Avishna-00@users.noreply.github.com>
2024-05-15 10:34:10 +0530

    mark-attendance route added

---
+++ routes/attendanceMarking.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+
+// Import database connection
+const { poolPromise } = require('../lib/database');
+
+// Import route middlewares
+const logger = require("../middleware/logger");
+const authenticateToken = require('../middleware/authenticateToken');
+
+// Use route middlewares
+router.use(logger);
+router.use(authenticateToken);
+
+router.route('/')
+	.get(async (req, res) => {
+		// const pool = await poolPromise;
+		// const result = await pool.request().query(`SELECT FirstName, LastName FROM Employee WHERE email = '${req.user.email}'`);
+		res.status(200).json({
+			// firstName: result.recordset[0].FirstName,
+			// lastName: result.recordset[0].LastName
+		});
+	});
+
+module.exports = router;
