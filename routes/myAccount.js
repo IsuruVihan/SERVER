f839c2c0894a9f256c438027d7b946c7f6983015
Avishna Arambawela <111332549+Avishna-00@users.noreply.github.com>
2024-05-19 16:45:32 +0530

    my account update error

--- routes/myAccount.js
+++ routes/myAccount.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+const multer = require('multer');
+
+// Set up multer middleware for handling multipart/form-data
+const upload = multer();
+
 // Import database connection
 const { poolPromise } = require('../lib/database');
 
@@ -16,15 +21,54 @@ router.route('')
 	.get(async (req, res) => {
 		try {
 			const pool = await poolPromise;
-			const result = await pool.request().query(`SELECT FirstName, LastName, Team, Role, ContactNumber, Email, BirthMonth, BirthDay FROM Employee WHERE Email = '${req.user.email}'`);
+			const result = await pool.request().query(`SELECT FirstName, LastName, Team, Role, ContactNumber, Email, Birthdate FROM Employee WHERE Email = '${req.user.email}'`);
 			res.status(200).json(result.recordset);
 		} catch (err) {
 			console.error('Database query error:', err);
 			res.status(500).json({ error: 'Internal Server Error' });
 		}
 	})
-	.post((req, res) => {
-		res.status(200).json({message: "POST /users"});
+	.post(upload.none(), async (req, res) => {
+		const data = req.body.FirstName;
+		
+		try {
+			// Construct the SQL query dynamically based on the received data
+            let updateQuery = `UPDATE Employee SET `;
+            const queryParams = [];
+
+            if (data.FirstName) {
+                updateQuery += `FirstName = @FirstName, `;
+                queryParams.push({ name: 'FirstName', value: data.FirstName });
+            }
+            if (data.LastName) {
+                updateQuery += `LastName = @LastName, `;
+                queryParams.push({ name: 'LastName', value: data.LastName });
+            }
+            if (data.ContactNumber) {
+                updateQuery += `ContactNumber = @ContactNumber, `;
+                queryParams.push({ name: 'ContactNumber', value: data.ContactNumber });
+            }
+            if (data.Birthdate) {
+                updateQuery += `Birthdate = @Birthdate, `;
+                queryParams.push({ name: 'Birthdate', value: data.Birthdate });
+            }
+
+            // Remove the trailing comma and space
+            updateQuery = updateQuery.slice(0, -2);
+            updateQuery += ` WHERE Email = @Email`;
+
+            const request = pool.request();
+            queryParams.forEach(param => {
+                request.input(param.name, param.value);
+            });
+            request.input('Email', req.user.email);
+			console.log("UPDATE QUERY: ", updateQuery);
+            await request.query(updateQuery);
+
+            return res.status(200).json({ message: "Profile updated successfully" });
+		} catch (err) {
+			return res.status(200).json({error: err});
+		}
 	});
 
 // router.route('/:email')
